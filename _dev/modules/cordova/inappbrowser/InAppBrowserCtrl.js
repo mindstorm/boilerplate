d292058de9decadaf6167d583630060a206be3e1
Paul Cervenka <pce@seso.at>
2015-12-21 09:41:29 +0100

    update modules

--- _dev/modules/cordova/inappbrowser/InAppBrowserCtrl.js
+++ _dev/modules/cordova/inappbrowser/InAppBrowserCtrl.js
@@ -3,124 +3,127 @@
  ****************************************************/
 
 app.controller("InAppBrowserCtrl", function ($scope) {
-    "use strict";
+  "use strict";
 
-    // CONTENTTYPE
-    $scope.contentTypeData = [{
-        name: "M4V",
-        value: "assets/dummy.m4v"
+  // clear output
+  
+
+  // CONTENTTYPE
+  $scope.contentTypeData = [{
+    name: "M4V",
+    value: "assets/dummy.m4v"
     }, {
-        name: "PNG",
-        value: "assets/dummy.png"
+    name: "PNG",
+    value: "assets/dummy.png"
     }, {
-        name: "HTML",
-        value: "assets/dummy.html"
+    name: "HTML",
+    value: "assets/dummy.html"
     }, {
-        name: "WWW",
-        value: "http://www.orf.at"
+    name: "WWW",
+    value: "http://www.orf.at"
     }];
-    $scope.contentType = $scope.contentTypeData[1];
-    $scope.updateContentType = function (item) {
-        $scope.contentType = item;
-    };
+  $scope.contentType = $scope.contentTypeData[1];
+  $scope.updateContentType = function (item) {
+    $scope.contentType = item;
+  };
 
-    // TARGET
-    $scope.targetData = [{
-        name: "SELF",
-        value: "_self"
+  // TARGET
+  $scope.targetData = [{
+    name: "SELF",
+    value: "_self"
     }, {
-        name: "BLANK",
-        value: "_blank"
+    name: "BLANK",
+    value: "_blank"
     }, {
-        name: "SYSTEM",
-        value: "_system"
+    name: "SYSTEM",
+    value: "_system"
     }];
-    $scope.target = $scope.targetData[1];
-    $scope.updateTarget = function (item) {
-        $scope.target = item;
-    };
+  $scope.target = $scope.targetData[1];
+  $scope.updateTarget = function (item) {
+    $scope.target = item;
+  };
 
-    // TRANSITION STYLE
-    $scope.transitionStyleData = [{
-        name: "Flip Horizontal",
-        value: "fliphorizontal"
+  // TRANSITION STYLE
+  $scope.transitionStyleData = [{
+    name: "Flip Horizontal",
+    value: "fliphorizontal"
     }, {
-        name: "Cross Dissolve",
-        value: "crossdissolve"
+    name: "Cross Dissolve",
+    value: "crossdissolve"
     }, {
-        name: "Cover Vertical",
-        value: "coververtical"
+    name: "Cover Vertical",
+    value: "coververtical"
     }];
-    $scope.transitionStyle = $scope.transitionStyleData[2];
-    $scope.updateTransitionStyle = function (item) {
-        $scope.transitionStyle = item;
-    };
+  $scope.transitionStyle = $scope.transitionStyleData[2];
+  $scope.updateTransitionStyle = function (item) {
+    $scope.transitionStyle = item;
+  };
 
-    // TOOLBAR POSITION
-    $scope.toolbarPositionData = [{
-        name: "Bottom",
-        value: "bottom"
+  // TOOLBAR POSITION
+  $scope.toolbarPositionData = [{
+    name: "Bottom",
+    value: "bottom"
     }, {
-        name: "Top",
-        value: "top"
+    name: "Top",
+    value: "top"
     }];
-    $scope.toolbarPosition = $scope.toolbarPositionData[0];
-    $scope.updateToolbarPosition = function (item) {
-        $scope.toolbarPosition = item;
-    };
+  $scope.toolbarPosition = $scope.toolbarPositionData[0];
+  $scope.updateToolbarPosition = function (item) {
+    $scope.toolbarPosition = item;
+  };
 
-    // LOCATION BAR
-    $scope.locationBar = "no";
-    $scope.updateLocationBar = function (item) {
-        $scope.locationBar = item;
-    };
+  // LOCATION BAR
+  $scope.locationBar = "no";
+  $scope.updateLocationBar = function (item) {
+    $scope.locationBar = item;
+  };
 
-    // TOOL BAR
-    $scope.toolBar = "yes";
-    $scope.updateToolBar = function (item) {
-        $scope.toolBar = item;
-    };
+  // TOOL BAR
+  $scope.toolBar = "yes";
+  $scope.updateToolBar = function (item) {
+    $scope.toolBar = item;
+  };
 
-    // LISTENER
-    $scope.iabListener = function (event) {
-        $scope.log(event.type);
+  // LISTENER
+  $scope.iabListener = function (event) {
+    $scope.log(event.type);
 
-        if (event.url === "iab://notfound") {
-            $scope.ref.close();
-        }
+    if (event.url === "iab://notfound") {
+      $scope.ref.close();
+    }
 
-    };
-    $scope.iabClose = function (event) {
-        $scope.log(event.type);
+  };
+  $scope.iabClose = function (event) {
+    $scope.log(event.type);
 
-        $scope.ref.removeEventListener("loadstart", $scope.iabListener);
-        $scope.ref.removeEventListener("loadstop", $scope.iabListener);
-        $scope.ref.removeEventListener("loaderror", $scope.iabListener);
-        $scope.ref.removeEventListener("exit", $scope.iabClose);
-    };
+    $scope.ref.removeEventListener("loadstart", $scope.iabListener);
+    $scope.ref.removeEventListener("loadstop", $scope.iabListener);
+    $scope.ref.removeEventListener("loaderror", $scope.iabListener);
+    $scope.ref.removeEventListener("exit", $scope.iabClose);
+  };
 
-    // OPEN
-    $scope.ref = null;
-    $scope.open = function (e) {
-        e.preventDefault();
+  // OPEN
+  $scope.ref = null;
+  $scope.open = function (e) {
+    e.preventDefault();
 
-        var options = [];
-        options.push("transitionstyle=" + $scope.transitionStyle.value);
-        options.push("location=" + $scope.locationBar);
-        options.push("toolbarposition=" + $scope.toolbarPosition.value);
+    var options = [];
+    options.push("transitionstyle=" + $scope.transitionStyle.value);
+    options.push("location=" + $scope.locationBar);
+    options.push("toolbarposition=" + $scope.toolbarPosition.value);
 
-        $scope.ref = window.open($scope.contentType.value, $scope.target.value, options.join());
-        $scope.ref.addEventListener("loadstart", $scope.iabListener);
-        $scope.ref.addEventListener("loadstop", $scope.iabListener);
-        $scope.ref.addEventListener("loaderror", $scope.iabListener);
-        $scope.ref.addEventListener("exit", $scope.iabClose);
+    $scope.ref = window.open($scope.contentType.value, $scope.target.value, options.join());
+    $scope.ref.addEventListener("loadstart", $scope.iabListener);
+    $scope.ref.addEventListener("loadstop", $scope.iabListener);
+    $scope.ref.addEventListener("loaderror", $scope.iabListener);
+    $scope.ref.addEventListener("exit", $scope.iabClose);
 
-        return false;
-    };
+    return false;
+  };
 
-    $scope.error = function (e) {
-        $scope.contentType.value = "FEHLER";
-        return $scope.open(e);
-    };
+  $scope.error = function (e) {
+    $scope.contentType.value = "FEHLER";
+    return $scope.open(e);
+  };
 
 });
